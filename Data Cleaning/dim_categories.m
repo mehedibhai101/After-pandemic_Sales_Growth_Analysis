743aa96f91b951cd88555c95ed9202035cc05eb5
Mehedi Hasan <mehedihasanamin7@gmail.com>
2026-02-12 01:55:35 +0600

    Create dim_categories.m

---
+++ Data Cleaning/dim_categories.m
@@ -0,0 +1,14 @@
+let
+    // Extracted the product category data from the CSV source.
+    Source = Csv.Document(File.Contents("C:\Users\perennial\OneDrive\Documents\Battle of Insights - PowerBI\Dataset\AdventureWorks Product Categories Lookup.csv"),[Delimiter=",", Columns=2, Encoding=1252, QuoteStyle=QuoteStyle.None]),
+
+    // Promoted the first row to headers to identify Category keys and names.
+    Promoted_Headers = Table.PromoteHeaders(Source, [PromoteAllScalars=true]),
+
+    // Assigned the correct data types to ensure proper relationship mapping in the data model.
+    Set_Data_Types = Table.TransformColumnTypes(Promoted_Headers,{{"ProductCategoryKey", Int64.Type}, {"CategoryName", type text}}),
+
+    // Renamed columns to follow a clean, professional naming convention for stakeholders.
+    Renamed_Columns = Table.RenameColumns(Set_Data_Types,{{"ProductCategoryKey", "Product Category Key"}, {"CategoryName", "Category Name"}})
+in
+    Renamed_Columns
